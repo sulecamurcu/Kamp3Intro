84421cdf3f46aa8ba51128ed7fb7250ad60b6b5b
şule-Bilgisayar\şule <scamurcu1905@gmail.com>
2021-01-31 19:52:14 +0200

    kamp 3 intro yüklendi

---
+++ Kamp3ıntro/Metotlar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string Aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun(); // Bir class tanımlamam için bu hareketi yapmam gerekiyor.Class örneği oluşturduk.ürün oluşturduk
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            
+            Urun[] urunler = new Urun[] {urun1,urun2 };//ürün arrayı oluşturduk.array;bir veri tipinde datayı tutan yapıdır.
+
+            foreach ( Urun urun in urunler) // "in ürünler" dedik.bu demek oluyor ki yukarıdaki urunler dizindeki verileri tek tek gezsin.foreach ile urunlere gelen datayı tek tek ekrana basarız.Yazdığımız "Urun" veri tipidir.Çalışacağı veri tipini bilecek abii...
+            {
+
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------------------------");
+
+
+
+            }
+
+            Console.WriteLine("---------------Metotlar--------------------");
+
+            SepetMenagar sepetMenagar = new SepetMenagar();
+
+            sepetMenagar.Ekle(urun1);
+            sepetMenagar.Ekle(urun2);
+            
+
+        }
+    }
+}
---
+++ Kamp3ıntro/Metotlar/SepetMenagar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetMenagar
+    {
+       // naming convention-isimlendirme kuralı
+       //syntax- yazım sekilleri
+       public void Ekle(Urun urun) //Bu bir imzadır.metot bir parametre alıyor o parametrenin tipi "Urun" urunun adını da aşağıdaki sekilde yazdırıyorum.
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi:"+urun.Adi);
+        }
+
+
+    }
+}
